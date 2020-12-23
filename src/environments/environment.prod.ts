69c47be4973923815f268066c19d2c0057a6d587
Ayan Kumar Saha <ayankumarsaha96@gmail.com>
2020-12-24 02:25:51 +0530

    Add deployment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: 'fec25aa109f543f781c9d873270451ab',
+  authEndPoint: 'https://accounts.spotify.com/authorize',
+  redirectUri: 'https://ayan-kumar-saha.github.io/SpotifyClone/',
 };
